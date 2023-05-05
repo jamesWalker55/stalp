6e095699c14bf260c5c1d29474ebdc7bf5fbf8d3
James Walker <james.chunho@gmail.com>
2023-05-05 17:48:15 +0100

    util file

---
+++ stalp/util.py
@@ -0,0 +1,10 @@
+import math
+
+
+def conv2d_length(length: int, kernel_length: int, padding: int = 0, stride: int = 1):
+    """
+    Formula described in:
+    https://pytorch.org/docs/stable/generated/torch.nn.Conv2d.html
+    """
+
+    return math.floor((length + 2 * padding - (kernel_length - 1) - 1) / stride + 1)
